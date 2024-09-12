6d842f95ac61241ac592cb8fc9f21b686cbe1577
Eduardo Yaguar <eduardoyaguarf@gmail.com>
2024-09-12 08:40:23 -0500

    Create Taller-4.pl

---
+++ Taller-4.pl
@@ -0,0 +1,19 @@
+
+materia('Matematicas', 3).
+materia('Base de datos', 3).
+materia('Analisis de algoritmos', 3).
+
+estudiante('Juan', 'Matematicas').
+estudiante('Andres', 'Base de datos').
+estudiante('Anna', 'Analisis de algoritmos').
+
+
+materiasxestudiante(Estudiante, Materia) :-
+    estudiante(Estudiante, Materia).
+
+estudiantesxmateria(Materia, Estudiante) :-
+    estudiante(Estudiante, Materia).
+
+creditosxestudiante(Estudiante, Creditos) :-
+    estudiante(Estudiante, Materia),
+    materia(Materia, Creditos).
